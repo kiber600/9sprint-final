6a1f65980328e6f839fa07333bf3aeae44d3c583
Viktor K <kiber600@gmail.com>
2025-11-22 21:10:39 +0700

    fist com

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -10,31 +13,109 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) []int {
+func generateRandomElements(size int) ([]int, error) {
 	// ваш код здесь
+	if size <= 0 {
+		return []int{}, fmt.Errorf("len of slice equal <=0")
+	}
+	src := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(src)
+	sliceOfInt := make([]int, size)
+	for i := 0; i < size; i++ {
+		sliceOfInt[i] = rng.Int()
+	}
+
+	return sliceOfInt, nil
 }
 
 // maximum returns the maximum number of elements.
-func maximum(data []int) int {
+func maximum(data []int) (int, error) {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0, fmt.Errorf("len of slice equal <=0")
+	}
+	if len(data) == 1 {
+		return data[0], nil
+	}
+	max := data[0]
+	for j := range data {
+		if max < data[j] {
+			max = data[j]
+		}
+	}
+	return max, nil
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		fmt.Println("Len of slice equal <=0")
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+	max := data[0]
+	for j := range data {
+		if max < data[j] {
+			max = data[j]
+		}
+	}
+	return max
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	data, err := generateRandomElements(SIZE)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
-
+	start := time.Now()
+	max, err := maximum(data)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+	elapsed := time.Since(start).Microseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
-
+	start = time.Now()
+	var wg sync.WaitGroup
+	result := make(chan int, CHUNKS)
+	stream := CHUNKS
+	for i := 0; i < stream; i++ {
+		if SIZE <= CHUNKS {
+			stream = 1
+		}
+		sliceStart := i * SIZE / stream
+		sliceEnd := sliceStart + SIZE/stream
+		if i == CHUNKS-1 {
+			sliceEnd = SIZE
+		}
+		sliceOfData := data[sliceStart:sliceEnd]
+		wg.Add(1)
+		go func(dataCh []int) {
+			defer wg.Done()
+			max := maxChunks(dataCh)
+			result <- max
+		}(sliceOfData)
+	}
+	wg.Wait()
+	close(result)
+	max = 0
+	for i := range result {
+		if i > max {
+			max = i
+		}
+	}
+	elapsed = time.Since(start).Microseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 // Пишите тесты в этом файле
+func TestGenerateRandomElements(t *testing.T) {
+	size := []int{10, 5, 12, 8, 0, 4, 7, -3}
+	for _, v := range size {
+		res, err := generateRandomElements(v)
+		if v <= 0 {
+			assert.Equal(t, []int{}, res)
+			assert.Error(t, err)
+
+		} else {
+			assert.Len(t, res, v)
+		}
+	}
+
+}
+
+func TestMaximum(t *testing.T) {
+	data := [][]int{
+		{0},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		{},
+		{5},
+		{25, 10, 3, 8, 44, 3, 3, 5, 25, 55},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{-5, -12, -4, -1, -155},
+	}
+	for i := 0; i < len(data); i++ {
+		m, err := maximum(data[i])
+		if len(data[i]) == 0 {
+			fmt.Printf("Expected: 0 Actual: %d\n", m) // Выводим ожидаемое значение для проверки в консоли (необязательно)
+			assert.Equal(t, 0, m)
+			assert.Error(t, err)
+		}
+		if len(data[i]) == 1 {
+			fmt.Printf("Expected: %d Actual: %d\n", data[i][0], m) // Выводим ожидаемое значение для проверки в консоли (необязательно)
+			assert.Equal(t, data[i][0], m)
+		}
+		if len(data[i]) > 1 {
+			max := data[i][0]
+			for j := 0; j < len(data[i]); j++ {
+				if data[i][j] > max {
+					max = data[i][j]
+
+				}
+			}
+			fmt.Printf("Expected: %d Actual: %d\n", max, m) // Выводим ожидаемое значение для проверки в консоли (необязательно)
+			assert.Equal(t, max, m)
+		}
+	}
+}
