b3fdcbd1ed4e29f62f2da75af9a3bd02ac4b0207
Viktor K <kiber600@gmail.com>
2025-11-23 20:12:02 +0700

    corrected

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) ([]int, error) {
+func generateRandomElements(size int) []int {
 	// ваш код здесь
 	if size <= 0 {
-		return []int{}, fmt.Errorf("len of slice equal <=0")
+		return []int{}
 	}
 	src := rand.NewSource(time.Now().UnixNano())
 	rng := rand.New(src)
@@ -25,32 +25,13 @@ func generateRandomElements(size int) ([]int, error) {
 		sliceOfInt[i] = rng.Int()
 	}
 
-	return sliceOfInt, nil
+	return sliceOfInt
 }
 
 // maximum returns the maximum number of elements.
-func maximum(data []int) (int, error) {
+func maximum(data []int) int {
 	// ваш код здесь
 	if len(data) == 0 {
-		return 0, fmt.Errorf("len of slice equal <=0")
-	}
-	if len(data) == 1 {
-		return data[0], nil
-	}
-	max := data[0]
-	for j := range data {
-		if max < data[j] {
-			max = data[j]
-		}
-	}
-	return max, nil
-}
-
-// maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
-	// ваш код здесь
-	if len(data) == 0 {
-		fmt.Println("Len of slice equal <=0")
 		return 0
 	}
 	if len(data) == 1 {
@@ -65,57 +46,49 @@ func maxChunks(data []int) int {
 	return max
 }
 
-func main() {
-	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+// maxChunks returns the maximum number of elements in a chunks.
+func maxChunks(data []int) int {
 	// ваш код здесь
-	data, err := generateRandomElements(SIZE)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-
-	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
-	start := time.Now()
-	max, err := maximum(data)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	elapsed := time.Since(start).Microseconds()
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
-	// ваш код здесь
-	start = time.Now()
 	var wg sync.WaitGroup
-	result := make(chan int, CHUNKS)
-	stream := CHUNKS
-	for i := 0; i < stream; i++ {
-		if SIZE <= CHUNKS {
-			stream = 1
-		}
-		sliceStart := i * SIZE / stream
-		sliceEnd := sliceStart + SIZE/stream
+	result := make([]int, CHUNKS)
+	for i := 0; i < CHUNKS; i++ {
+
+		sliceStart := i * SIZE / CHUNKS
+		sliceEnd := sliceStart + SIZE/CHUNKS
 		if i == CHUNKS-1 {
 			sliceEnd = SIZE
 		}
 		sliceOfData := data[sliceStart:sliceEnd]
 		wg.Add(1)
-		go func(dataCh []int) {
+		go func(idx int, dataCh []int) {
 			defer wg.Done()
-			max := maxChunks(dataCh)
-			result <- max
-		}(sliceOfData)
+			max := maximum(dataCh)
+			result[idx] = max
+
+		}(i, sliceOfData)
+
 	}
 	wg.Wait()
-	close(result)
-	max = 0
-	for i := range result {
-		if i > max {
-			max = i
-		}
-	}
+	return maximum(result)
+}
+
+func main() {
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	// ваш код здесь
+	data := generateRandomElements(SIZE)
+
+	fmt.Println("Ищем максимальное значение в один поток")
+	// ваш код здесь
+	start := time.Now()
+	max := maximum(data)
+	elapsed := time.Since(start).Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	// ваш код здесь
+
+	start = time.Now()
+	max = maxChunks(data)
 	elapsed = time.Since(start).Microseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -9,14 +9,13 @@ import (
 
 // Пишите тесты в этом файле
 func TestGenerateRandomElements(t *testing.T) {
-	size := []int{10, 5, 12, 8, 0, 4, 7, -3}
+	size := []int{10, 5, 12, 8, 0, 4, 7, -3, 500, 755, 5000, 125, 324, 748, 10124, 15725, 250531, 1, 155, 324, 747, 89, 66}
 	for _, v := range size {
-		res, err := generateRandomElements(v)
+		res := generateRandomElements(v)
 		if v <= 0 {
 			assert.Equal(t, []int{}, res)
-			assert.Error(t, err)
-
 		} else {
+			fmt.Printf("Expected: %d Actual: %d len of slice\n", v, len(res)) // Выводим ожидаемое значение для проверки в консоли (необязательно)
 			assert.Len(t, res, v)
 		}
 	}
@@ -31,14 +30,13 @@ func TestMaximum(t *testing.T) {
 		{5},
 		{25, 10, 3, 8, 44, 3, 3, 5, 25, 55},
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{-5, -12, -4, -1, -155},
+		{500, 7000, 1555500, 1212121, 32, 158},
 	}
 	for i := 0; i < len(data); i++ {
-		m, err := maximum(data[i])
+		m := maximum(data[i])
 		if len(data[i]) == 0 {
 			fmt.Printf("Expected: 0 Actual: %d\n", m) // Выводим ожидаемое значение для проверки в консоли (необязательно)
 			assert.Equal(t, 0, m)
-			assert.Error(t, err)
 		}
 		if len(data[i]) == 1 {
 			fmt.Printf("Expected: %d Actual: %d\n", data[i][0], m) // Выводим ожидаемое значение для проверки в консоли (необязательно)
